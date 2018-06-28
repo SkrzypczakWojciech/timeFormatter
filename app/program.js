b532b92cde66d732c7c536d362e7b5f0c486a2d7
Wojciech Skrzypczak <skrzypczakwojciech@wp.pl>
2018-06-28 22:11:40 +0200

    change in program.js

--- app/program.js
+++ app/program.js
@@ -1,6 +1,5 @@
 var os = require('os');
 var OSinfo = require('../modules/OSinfo');
-var timeformatter = require('../modules/timeformatter');
 
 process.stdin.setEncoding('utf-8');
 process.stdout.write('Witaj, podaj komende' + '\n')
@@ -18,7 +17,6 @@ process.stdin.on('readable', function() {
                 break;
             case '/timeformatter':
                 process.stdout.write('Podaj liczbe sekund\n');
-                timeformatter.print();
                 break;
             case '/getOSinfo':
             	process.stdout.write('info o systemie')
